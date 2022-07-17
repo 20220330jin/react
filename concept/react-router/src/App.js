c3c1c723eee6e774135b9796bf53c459c88de0ac
jin <20220330jin@gmail.com>
2022-07-18 07:57:26 +0900

    [Section20] 라우터 작업 생성

---
+++ concept/react-router/src/App.js
@@ -0,0 +1,20 @@
+import { Route } from 'react-router-dom';
+
+import Welcome from './components/Welcome';
+import Products from './components/Products';
+
+function App() {
+  return (
+    <div>
+      <div>dfsdafsdf</div>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
+    </div>
+  );
+}
+
+export default App;
