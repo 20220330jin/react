0c6585b02eaf9799137a84c06fc71832e4ee3a2b
jin <20220330jin@gmail.com>
2022-07-20 19:21:35 +0900

    [Section3] 컴포넌트 나누기 및 props 이용

--- concept/react-complete-guide/src/App.js
+++ concept/react-complete-guide/src/App.js
@@ -1,11 +1,15 @@
-import ExpenseItem from "./components/ExpenseItem";
+import Expenses from "./components/Expenses";
 
 function App() {
+  const expenses = [
+    { title: 'Car Insurance', amount: 3002, date: new Date(2022, 3, 24) },
+    { title: 'Parts Insurance', amount: 422, date: new Date(2022, 4, 24) },
+    { title: 'Laptop Insurance', amount: 1233, date: new Date(2022, 5, 24) }
+  ];
   return (
     <div>
       <h2>Let's get start!</h2>
-      <p>This is also Available</p>
-      <ExpenseItem></ExpenseItem>
+      <Expenses items={expenses}></Expenses>
     </div>
   );
 }
