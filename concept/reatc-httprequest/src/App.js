3b989ca425add5cdd3ee1cf2829dd8cd5bfdc1cd
jin <20220330jin@gmail.com>
2022-08-08 08:34:04 +0900

    [Section14] http request - POST

--- concept/reatc-httprequest/src/App.js
+++ concept/reatc-httprequest/src/App.js
@@ -13,35 +13,50 @@ function App() {
     setIsloading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch('https://react-http-a3c85-default-rtdb.firebaseio.com/movie.json');
 
       const data = await response.json();
+      console.log(data);
+
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate
+        })
+      }
 
       if (!response.ok) {
         throw new Error('Something went wrong!');
       }
 
-      const transformdMovies = data.results.map(movieData => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date
-        }
-      });
-      setMovies(transformdMovies);
+      setMovies(loadedMovies);
     } catch (error) {
       setError(error.message);
     }
     setIsloading(false);
   }, []);
 
+  console.log(fetchMoviesHandler);
+
   useEffect(() => {
     fetchMoviesHandler();
   }, [fetchMoviesHandler])
 
-  function addMovieHandler(movie) {
+  async function addMovieHandler(movie) {
     console.log(movie);
+    const response = await fetch('https://react-http-a3c85-default-rtdb.firebaseio.com/movie.json', {
+      method: 'POST',
+      body: JSON.stringify(movie),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await response.json();
+    console.log(data);
   }
 
   return (
