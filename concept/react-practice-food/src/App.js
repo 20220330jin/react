6d1b2c6fa7fbe7096962a9c9930b78ba7594c757
jin <20220330jin@gmail.com>
2022-08-10 08:33:40 +0900

    [Section11] create project

---
+++ concept/react-practice-food/src/App.js
@@ -0,0 +1,16 @@
+import React, { Fragment } from "react";
+
+import Header from "./components/Layout/Header";
+
+
+function App() {
+  return (
+    <div>
+      <Fragment>
+        <Header></Header>
+      </Fragment>
+    </div>
+  );
+}
+
+export default App;
