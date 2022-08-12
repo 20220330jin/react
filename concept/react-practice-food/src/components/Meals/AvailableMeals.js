bc1e0ff05f8d1e70bd90473dc234c216786bba62
jin <20220330jin@gmail.com>
2022-08-12 19:46:26 +0900

    [Section16] Loading

--- concept/react-practice-food/src/components/Meals/AvailableMeals.js
+++ concept/react-practice-food/src/components/Meals/AvailableMeals.js
@@ -7,8 +7,10 @@ import MealItem from './MealItem/MealItem';
 const AvailableMeals = () => {
 
     const [meals, setMeals] = useState([]);
+    const [isLoading, setLoading] = useState(false);
 
     const fetchMealsHandler = useCallback(async () => {
+        setLoading(true);
         try {
             const response = await fetch('https://react-http-a3c85-default-rtdb.firebaseio.com/meals.json')
             const data = await response.json();
@@ -24,6 +26,7 @@ const AvailableMeals = () => {
                 })
             }
             setMeals(loadedMeals);
+            setLoading(false);
             console.log(meals);
         } catch (error) {
             console.log(error);
@@ -33,6 +36,16 @@ const AvailableMeals = () => {
     useEffect(() => {
         fetchMealsHandler();
     }, [fetchMealsHandler])
+
+    if (isLoading) {
+        return (
+            <section className={classes.MealsLoading}>
+                <p>Loading...</p>
+            </section>
+        )
+    }
+
+
 
 
     const mealsList = meals.map((meal) => <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price}></MealItem>)
