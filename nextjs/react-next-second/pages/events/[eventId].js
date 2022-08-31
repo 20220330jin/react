b6dec507c8551252abfbf9398f7238d3fe518b8b
jin <20220330jin@gmail.com>
2022-09-01 08:20:38 +0900

    [next-Section4] Event Detail Page and Header

--- nextjs/react-next-second/pages/events/[eventId].js
+++ nextjs/react-next-second/pages/events/[eventId].js
@@ -1,8 +1,34 @@
+import {useRouter} from "next/router";
+import {getEventById} from "../../dummy-data";
+import {Fragment} from "react";
+import EventSummary from "../../components/event-detail/event-summary";
+import EventLogistics from "../../components/event-detail/event-logistics";
+import EventContent from "../../components/event-detail/event-content";
+
 function EventDetailPage() {
+
+    const router = useRouter();
+    console.log(router.query);
+
+    const eventId = router.query.eventId;
+    const event = getEventById(eventId);
+    console.log(event);
+
+    if (!event) {
+        return (
+            <p>No Event Found!</p>
+        )
+    }
+
     return (
-        <div>
-            <h1>Event Detail</h1>
-        </div>
+        <Fragment>
+            <EventSummary title={event.title}></EventSummary>
+            <EventLogistics date={event.date} address={event.location} image={event.image}
+                            imageAlt={event.title}></EventLogistics>
+            <EventContent>
+                <p>{event.description}</p>
+            </EventContent>
+        </Fragment>
     )
 }
 
