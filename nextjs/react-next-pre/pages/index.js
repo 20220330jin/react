addee2fb2e494a42b7a242f3292f6fca24564a45
jin <20220330jin@gmail.com>
2022-09-07 08:38:52 +0900

    [next-Section5] 동적 매개변수 작업

--- nextjs/react-next-pre/pages/index.js
+++ nextjs/react-next-pre/pages/index.js
@@ -1,3 +1,7 @@
+import path from 'path';
+import fs from 'fs/promises';
+import Link from "next/link";
+
 function HomePage(props) {
 
     const {products} = props;
@@ -5,17 +9,39 @@ function HomePage(props) {
     return (
         <ul>
             {products.map(product => (
-                <li key={product.id}>{product.title}</li>
+                <li key={product.id}><Link href={`/${product.id}`}>{product.title}</Link></li>
             ))}
         </ul>
     );
 }
 
-export async function getStaticProps() {
+export async function getStaticProps(context) {
+    const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json') //current working directory
+    console.log('re-render');
+    console.log(context);
+    console.log(process.cwd());
+    const jsonData = await fs.readFile(filePath);
+    const data = JSON.parse(jsonData);
+
+    // if (data.products.length === 0) {
+    //     return {notFound: true};
+    // }
+
+    if (!data) {
+        return {
+            redirect: {
+                destination: '/no-data'
+            }
+        }
+    }
+
     return {
         props: {
-            products: [{id: 'p1', title: 'Product1'}]
-        }
+            products: data.products
+        },
+        revalidate: 10,
+        // notFound: true,
+        // redirect:
     };
 }
 
